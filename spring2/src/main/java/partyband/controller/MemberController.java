507b91b97b4d46aa53b769e490b7361eecea0de2
accotin <accotin12@gmail.com>
2022-11-07 14:13:27 +0900

    1107어드민 추가(수정)

--- spring2/src/main/java/partyband/controller/MemberController.java
+++ spring2/src/main/java/partyband/controller/MemberController.java
@@ -291,12 +291,8 @@ public class MemberController {
 			
 			return "member/loginResult";
 			
-		} else if(member.getMember_id().equals("admin")){// 관리자 일떄
-				session.setAttribute("admin", member);
-				
-				return "redirect:partyband.do"; 
-			
-		}else if (member.getMember_passwd().equals(pwd)) {// 비번이 같을때
+		} else {			// 등록된 회원일때
+			if (member.getMember_passwd().equals(pwd)) {// 비번이 같을때
 				session.setAttribute("member", member);
 
 				String member_file = member.getMember_file();
@@ -313,7 +309,7 @@ public class MemberController {
 			}
 		}
 
-
+	}
 	
 	/* 회원 마이 페이지 */
 	@RequestMapping(value = "/member_mypage.do")
